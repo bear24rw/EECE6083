e3a647296b07b911122423356ec4c52dd9b6dec2
Max Thrun <bear24rw@gmail.com>
2014-04-14 22:44:57 -0400

    restore fp in runtime funcs

--- runtime_inline.c
+++ runtime_inline.c
@@ -2,10 +2,12 @@ putinteger:
     R[0] = M[FP];
     putInteger(R[0]);
     R[0] = M[FP-2];
+    FP = M[FP-1];
     goto *(void *)R[0];
 
 putbool:
     R[0] = M[FP];
     putBool(R[0]);
     R[0] = M[FP-2];
+    FP = M[FP-1];
     goto *(void *)R[0];
